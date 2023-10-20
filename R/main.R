4a2d4d43bd270043dacb782f256d86db6c08c582
halterc <chalterbio@gmail.com>
2023-10-20 12:37:39 +0200

    Simplified, generalized

--- R/main.R
+++ R/main.R
@@ -1,81 +1,48 @@
-# Extracts the broad cell type and subtype composition from scGate and ProjecTIL annotated datasets
-# Calculates relative abundance (freq) and clr-transformed freq
+# Calculates:
+# - Cell counts
+# - relative abundance (freq, sums to 100% for each metadata column provided)
+# - clr-transformed relative abundance (freq_clr)
 
-aux <- file.path(getwd(), "aux")
-
-## Use this to add new maps with new subtypes
-# CD8T_human_ref_v1 <- load.reference.map("~/Dropbox/CSI/reference_atlases/CD8T_human_ref_v1.rds")
-# CD4T_human_ref_v2 <- load.reference.map("~/Dropbox/CSI/reference_atlases/CD4T_human_ref_v2.rds")
-# DC_human_ref_v1 <- load.reference.map("~/Dropbox/CSI/reference_atlases/DC_human_ref_v1.rds")
-# MoMac_human_v1 <- load.reference.map("~/Dropbox/CSI/reference_atlases/MoMac_human_v1.rds")
-# map.subtypes <- c()
-# for(map in maps){
-#   map.subtypes <- c(levels(get(map)$functional.cluster))
-#   # Need to add "\n" (line break) at the end, otherwise read.csv will complain
-#   cat(c(paste0(map.subtypes, collapse = ","), "\n"), file = file.path(aux, sprintf("%s.txt", map)), sep = "")
-# }
-
-# Default maps
-m <- c("CD8T_human_ref_v1","CD4T_human_ref_v2", "DC_human_ref_v1", "MoMac_human_v1")
-
-calc_CTcomp <- function(object, sample.col = "Sample", meta.df, maps = m,
-                        min.cells = 10, useNA = TRUE){
-  # Check maps input
-  maps.available <- stringr::str_remove(list.files(aux), ".txt")
-  if (any(!maps %in% maps.available)) {
-    stop(paste0(maps[!maps %in% maps.available], " not found in available reference maps. Available reference maps:\n", paste(maps.available, collapse = "\n")))
-  }
-  
-  # Broad cell types
-  celltype.compositions <- list()
-  celltype.compositions$sample_metadata <- meta.df
-  
+calc_CTcomp <- function(object, sample.col = NULL, annot.cols = "scGate_multi",
+                        min.cells = 10, useNA = TRUE, rename.Multi = F){
   useNA <- ifelse(useNA == TRUE, "always", "no")
-  object$scGate_multi[object$scGate_multi == "Multi"] <- NA
-  broadtype_comp_table <- table(object$scGate_multi,
-                                object@meta.data[[sample.col]],
-                                useNA = useNA)
-  broadtype_comp_table_freq <- prop.table(broadtype_comp_table+1, margin = 2)
   
-  ## clr-transform
-  broadtype_comp_table_clr <- Hotelling::clr(t(broadtype_comp_table_freq))
+  celltype.compositions <- list()
   
-  ## Append
-  celltype.compositions$broad_cell_types[["cell_counts"]] <- as.data.frame.matrix(t(broadtype_comp_table))
-  celltype.compositions$broad_cell_types[["freq"]] <- as.data.frame.matrix(t(broadtype_comp_table_freq))
-  celltype.compositions$broad_cell_types[["freq_clr"]] <- as.data.frame.matrix(broadtype_comp_table_clr)
-  
-  
-  # High-resolution subtypes
-  
-  ## Get subtypes for specific reference map version
-  map.subtypes <- list()
-  for(map in maps){
-    map.subtypes[[map]] <- unlist(read.csv(file.path(aux, sprintf("%s.txt", map)), header = F))
-  }
-  for(map in maps){
-    print(paste("Used subtypes from reference map:  ", map))
-    subtypes <- map.subtypes[[map]]
-    subtype_comp_table <- table(object$functional.cluster[which(object$functional.cluster %in% subtypes)],
-                                object$Sample[which(object$functional.cluster %in% subtypes)],
-                                useNA = useNA)
-    subtype_comp_table <- subtype_comp_table[-nrow(subtype_comp_table),-ncol(subtype_comp_table)] 
+  for (annot.col in annot.cols) {
+    if (length(pbmc3k@meta.data[[annot.cols]]) == 0) {
+      stop(paste("Annotation metadata column", annot.col,"could not be found"))
+    }
     
-    if (sum(subtype_comp_table) < min.cells) {
-      warning(paste("Using the", map, "there are less than", min.cells, "cells detected, too few to calculate a reasonable celltype composition, please check."))
+    if (rename.Multi) {
+      object@meta.data[[annot.col]][object@meta.data[[annot.col]] == "Multi"] <- NA
+    }
+    
+    if (is.null(sample.col)) {
+      comp_table <- table(object@meta.data[[annot.col]],
+                          useNA = useNA)
+    } else {
+      tryCatch(invisible(length(object@meta.data[[sample.col]])), error = function(e){ stop(paste("Sample column could not be found"))})
+      comp_table <- table(object@meta.data[[annot.col]],
+                          object@meta.data[[sample.col]],
+                          useNA = useNA)
+    }
+
+    comp_table_freq <- prop.table(comp_table+1)
+    
+    if (sum(comp_table) < min.cells) {
+      warning(paste("There are less than", min.cells,
+                    "cells detected. This is too few to calculate a reasonable celltype composition, please check or adjust parameter min.cells."))
       next
     }
     
-    subtype_comp_table_freq <- prop.table(subtype_comp_table+1, margin = 2)
-    
     ## clr-transform
-    subtype_comp_table_clr <- Hotelling::clr(t(subtype_comp_table_freq))
+    comp_table_clr <- Hotelling::clr(t(comp_table_freq))
     
     ## Append
-    map.name <- strsplit(map, "_")[[1]][1]
-    celltype.compositions$subtypes[[map.name]][["cell_counts"]] <- as.data.frame.matrix(t(subtype_comp_table))
-    celltype.compositions$subtypes[[map.name]][["freq"]] <- as.data.frame.matrix(t(subtype_comp_table_freq))
-    celltype.compositions$subtypes[[map.name]][["freq_clr"]] <- as.data.frame.matrix(subtype_comp_table_clr)
+    celltype.compositions[[annot.col]][["cell_counts"]] <- as.data.frame.matrix(t(comp_table))
+    celltype.compositions[[annot.col]][["freq"]] <- as.data.frame.matrix(t(comp_table_freq))
+    celltype.compositions[[annot.col]][["freq_clr"]] <- as.data.frame.matrix(comp_table_clr)
   }
   return(celltype.compositions)
 }
